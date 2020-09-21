837cc8b575d5875ab5c8fee2692bded16caa29e3
Gabriel de Jesus de <gabriel.franca@dce.ufpb.br>
2020-09-21 18:26:30 +0000

    não esta funcionando no replit, teste no github

--- cupom_test.py
+++ cupom_test.py
@@ -1,20 +1,33 @@
-import cupom;
-
-nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
-logradouro = "Av. Projetada Leste"
-numero = 500
-complemento = "EUC F32/33/34"
-bairro = "Br. Sta Genebra"
-municipio = "Campinas"
-estado = "SP"
-cep = "13080-395"
-telefone = "(19) 3756-7408"
-observacao = "Loja 1317 (PDP)"
-cnpj = "42.591.651/0797-34"
-inscricao_estadual = "244.898.500.113"
+import cupom
 
 def test_exercicio1():
-    assert cupom.imprime_dados_loja() == '''Arcos Dourados Com. de Alimentos LTDA
+		global nome_loja
+		global logradouro
+		global numero
+		global complemento
+		global bairro
+		global municipio
+		global estado
+		global cep
+		global telefone
+		global observacao
+		global cnpj
+		global inscricao_estadual
+
+		nome_loja = "Arcos Dourados Com. de Alimentos LTDA"
+		logradouro = "Av. Projetada Leste"
+		numero = 500
+		complemento = "EUC F32/33/34"
+		bairro = "Br. Sta Genebra"
+		municipio = "Campinas"
+		estado = "SP"
+		cep = "13080-395"
+		telefone = "(19) 3756-7408"
+		observacao = "Loja 1317 (PDP)"
+		cnpj = "42.591.651/0797-34"
+		inscricao_estadual = "244.898.500.113"
+
+		assert cupom.imprime_dados_loja() == '''Arcos Dourados Com. de Alimentos LTDA
 Av. Projetada Leste, 500 EUC F32/33/34
 Br. Sta Genebra - Campinas - SP
 CEP:13080-395 Tel (19) 3756-7408
@@ -23,34 +36,35 @@ CNPJ: 42.591.651/0797-34
 IE: 244.898.500.113
 '''
 
-def test_exercicio2_tudo_vazio():
-    global nome_loja
-    global logradouro
-    global numero
-    global complemento
-    global bairro
-    global municipio
-    global estado
-    global cep
-    global telefone
-    global observacao
-    global cnpj
-    global inscricao_estadual
-    
-    nome_loja = ""
-    logradouro = ""
-    numero = 0
-    complemento = ""
-    bairro = ""
-    municipio = ""
-    estado = ""
-    cep = ""
-    telefone = ""
-    observacao = ""
-    cnpj = ""
-    inscricao_estadual = ""
 
-    assert cupom.imprime_dados_loja() == '''
+def test_exercicio2_tudo_vazio():
+		global nome_loja
+		global logradouro
+		global numero
+		global complemento
+		global bairro
+		global municipio
+		global estado
+		global cep
+		global telefone
+		global observacao
+		global cnpj
+		global inscricao_estadual
+
+		nome_loja = ""
+		logradouro = ""
+		numero = 0
+		complemento = ""
+		bairro = ""
+		municipio = ""
+		estado = ""
+		cep = ""
+		telefone = ""
+		observacao = ""
+		cnpj = ""
+		inscricao_estadual = ""
+
+		assert cupom.imprime_dados_loja() == '''
 , 0 
  -  - 
 CEP: Tel 
@@ -59,34 +73,41 @@ CNPJ:
 IE: 
 '''
 
-def test_exercicio2_customizado():
-    global nome_loja
-    global logradouro
-    global numero
-    global complemento
-    global bairro
-    global municipio
-    global estado
-    global cep
-    global telefone
-    global observacao
-    global cnpj
-    global inscricao_estadual
-    
-    # Defina seus próprios valores para as variáveis a seguir
-    nome_loja = ""
-    logradouro = ""
-    numero = 0
-    complemento = ""
-    bairro = ""
-    municipio = ""
-    estado = ""
-    cep = ""
-    telefone = ""
-    observacao = ""
-    cnpj = ""
-    inscricao_estadual = ""
 
-    #E atualize o texto esperado abaixo
-    assert cupom.imprime_dados_loja() == '''
+def test_exercicio2_customizado():
+		global nome_loja
+		global logradouro
+		global numero
+		global complemento
+		global bairro
+		global municipio
+		global estado
+		global cep
+		global telefone
+		global observacao
+		global cnpj
+		global inscricao_estadual
+
+		# Defina seus próprios valores para as variáveis a seguir
+		nome_loja = "Tropical"
+		logradouro = "Rua siqueira Campos"
+		numero = 580
+		complemento = ""
+		bairro = "Centro"
+		municipio = "Paulista"
+		estado = "Pernambuco"
+		cep = "53401-320"
+		telefone = "(81) 3438-5714"
+		observacao = ""
+		cnpj = "37.886.772/0001-82"
+		inscricao_estadual = "4232303-79"
+
+		#E atualize o texto esperado abaixo
+		assert cupom.imprime_dados_loja() == '''Tropical
+Rua siqueira Campos, 580 
+Centro - Paulista - Pernambuco
+CEP:53401-320 Tel (81) 3438-5714
+
+CNPJ: 37.886.772/0001-82
+IE: 4232303-79
 '''
